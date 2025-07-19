66d71f0411f4c99a38da325d23dbe0b2a8065f4c
DouglasLopesDRL <douglasribeirodrl@gmail.com>
2025-07-18 21:46:00 -0300

    changed full mai.js

---
+++ main.js
@@ -0,0 +1,8 @@
+//Capturando os elementos do html
+const form = document.getElementById('formulario');
+const valor1 = document.getElementById('iValor1');
+const valor2 = document.getElementById('iValor2');
+
+form.addEventListener('submit', function(e){
+    
+})
